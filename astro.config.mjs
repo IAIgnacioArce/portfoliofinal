7299cd221b3c4ce78c0642cbf6357f5bab804214
IAIgnacioArce <nachitoarce11@gmail.com>
2025-03-20 00:17:45 -0300

    Eliminado astro-tunnel y actualizado la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import { URL } from './src/data/constants'
 
-import tunnel from 'astro-tunnel'
 import icon from 'astro-icon'
 import { astroImageTools } from 'astro-imagetools'
 import i18n from '@astrolicious/i18n'
@@ -21,7 +20,6 @@ export default defineConfig({
   },
   compressHTML: false,
   integrations: [
-    tunnel(),
     icon(),
     astroImageTools,
     i18n({
